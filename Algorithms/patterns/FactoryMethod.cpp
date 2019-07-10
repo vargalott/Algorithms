06567c72c99a176b21b2b395e0674be25171bc6f
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-10 20:00:53 +0300

    Added FactoryMethod implementation

---
+++ Algorithms/patterns/FactoryMethod.cpp
@@ -0,0 +1,45 @@
+#include "FactoryMethod.hpp"
+
+#pragma region Products
+void DefaultProduct::DoStuff(void)
+{
+	std::cout << "\nDefault Product: doing stuff...";
+}
+void ConcreteProduct1::DoStuff(void)
+{
+	std::cout << "\nConcrete Product 1: doing stuff...";
+}
+void ConcreteProduct2::DoStuff(void)
+{
+	std::cout << "\nConcrete Product 2: doing stuff...";
+}
+#pragma endregion
+
+#pragma region Creators
+IProduct* Creator::CreateProduct(void) noexcept
+{
+	return new DefaultProduct();
+}
+ConcreteProduct1* ConcreteCreator1::CreateProduct(void) noexcept
+{
+	return new ConcreteProduct1();
+}
+ConcreteProduct2* ConcreteCreator2::CreateProduct(void) noexcept
+{
+	return new ConcreteProduct2();
+}
+#pragma endregion
+
+#pragma region TEST
+void TEST::DO_TEST(void)
+{
+	auto SomeOperation = [](Creator* creator) -> void
+	{
+		std::cout << "\nCreator undefined, but: ";
+		creator->CreateProduct()->DoStuff();
+	};
+	SomeOperation(new Creator());
+	SomeOperation(new ConcreteCreator1());
+	SomeOperation(new ConcreteCreator2());
+}
+#pragma endregion
---
+++ Algorithms/patterns/FactoryMethod.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#ifndef __FACTORY_METHOD_HPP__
+#define __FACTORY_METHOD_HPP__
+
+// for work demonstration
+#include <iostream>
+#include <string>
+
+#pragma region IProduct
+class IProduct
+{
+public:
+	virtual void DoStuff(void) = 0;
+};
+#pragma endregion
+
+#pragma region Products
+class DefaultProduct : public IProduct
+{
+public:
+	virtual void DoStuff(void) override final;
+};
+class ConcreteProduct1 : public IProduct
+{
+public:
+	virtual void DoStuff(void) override final;
+};
+class ConcreteProduct2 : public IProduct
+{
+public:
+	virtual void DoStuff(void) override final;
+};
+#pragma endregion
+
+#pragma region Creators
+class Creator
+{
+public:
+	Creator(void) = default;
+	~Creator(void) = default;
+
+	//void SomeOperation(void);
+	virtual IProduct* CreateProduct(void) noexcept;
+};
+
+class ConcreteCreator1 : public Creator
+{
+public:
+	virtual ConcreteProduct1* CreateProduct(void) noexcept override final;
+};
+class ConcreteCreator2 : public Creator
+{
+public:
+	virtual ConcreteProduct2* CreateProduct(void) noexcept override final;
+};
+#pragma endregion
+
+#pragma region TEST
+class TEST
+{
+public:
+	// use TEST::DO_TEST() for test
+	static void DO_TEST(void);
+};
+#pragma endregion
+
+#endif // !__FACTORY_METHOD_HPP__
