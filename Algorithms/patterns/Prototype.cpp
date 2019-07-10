0b657057d12c542b58b89854c4ef146ba0c353ac
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-10 19:31:02 +0300

    Added Prototype implementation

---
+++ Algorithms/patterns/Prototype.cpp
@@ -0,0 +1,35 @@
+#include "Prototype.hpp"
+
+#pragma region ConcretePrototype
+ConcretePrototype::ConcretePrototype(void)
+{
+	this->some_field_1 = "ConcretePrototype: some_field_1";
+	this->some_field_2 = 65536;
+	this->some_field_3 = 3.141592;
+}
+IPrototype* ConcretePrototype::Clone(void) noexcept
+{
+	IPrototype* cloned = new ConcretePrototype();
+	cloned = this; // do necessary things to copy itself
+	return cloned;
+}
+void ConcretePrototype::PrintData(void) const noexcept
+{
+	std::cout << "\nfield_1: " << this->some_field_1;
+	std::cout << "\nfield_2: " << this->some_field_2;
+	std::cout << "\nfield_3: " << this->some_field_3;
+}
+#pragma endregion
+
+#pragma region TEST
+void TEST::DO_TEST(void)
+{
+	ConcretePrototype existing;
+	std::cout << "\nexisting data:";
+	existing.PrintData();
+
+	ConcretePrototype* cloned = dynamic_cast<ConcretePrototype*>(existing.Clone());
+	std::cout << "\ncloned data:";
+	cloned->PrintData();
+};
+#pragma endregion
---
+++ Algorithms/patterns/Prototype.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef __PROTOTYPE_HPP__
+#define __PROTOTYPE_HPP__
+
+// for work demonstration
+#include <iostream>
+#include <string>
+
+#pragma region IPrototype
+class IPrototype
+{
+public:
+	virtual IPrototype* Clone(void) noexcept = 0;
+};
+#pragma endregion
+
+#pragma region ConcretePrototype
+class ConcretePrototype : public IPrototype
+{
+public:
+	ConcretePrototype(void);
+	~ConcretePrototype(void) = default;
+
+	virtual IPrototype* Clone(void) noexcept override final;
+	void PrintData(void) const noexcept;
+private:
+	std::string some_field_1;
+	ptrdiff_t some_field_2;
+	float some_field_3;
+};
+#pragma endregion
+
+#pragma region TEST
+class TEST
+{
+public:
+	// use TEST::DO_TEST() for test
+	static void DO_TEST(void);
+};
+#pragma endregion
+
+#endif // !__PROTOTYPE_HPP__
