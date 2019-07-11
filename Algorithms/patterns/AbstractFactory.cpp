c1512a3edba494e76d61d092a0b8bbe328455ed6
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-11 11:50:41 +0300

    Added AbstractFactory implementation

---
+++ Algorithms/patterns/AbstractFactory.cpp
@@ -0,0 +1,74 @@
+#include "AbstractFactory.hpp"
+
+#pragma region Products
+void ConcreteProductA1::SomeOperation(void)
+{
+	std::cout << "\nConcrete Product A1: doing something...";
+}
+void ConcreteProductA2::SomeOperation(void)
+{
+	std::cout << "\nConcrete Product A2: doing something...";
+}
+void ConcreteProductB1::SomeOperation(void)
+{
+	std::cout << "\nConcrete Product B1: doing something...";
+}
+void ConcreteProductB2::SomeOperation(void)
+{
+	std::cout << "\nConcrete Product B2: doing something...";
+}
+#pragma endregion
+
+#pragma region Factories
+IProduct* ConcreteFactory1::CreateProduct1(void) noexcept
+{
+	return new ConcreteProductA1();
+}
+IProduct* ConcreteFactory1::CreateProduct2(void) noexcept
+{
+	return new ConcreteProductA2();
+}
+IProduct* ConcreteFactory2::CreateProduct1(void) noexcept
+{
+	return new ConcreteProductB1();
+}
+IProduct* ConcreteFactory2::CreateProduct2(void) noexcept
+{
+	return new ConcreteProductB2();
+}
+#pragma endregion
+
+#pragma region Client
+Client::Client(IAbstractFactory& factory)
+{
+	this->current_factory = &factory;
+}
+void Client::ChangeFactory(IAbstractFactory& factory)
+{
+	this->current_factory = &factory;
+}
+IProduct* Client::CreateProduct1(void) noexcept
+{
+	return this->current_factory->CreateProduct1();
+}
+IProduct* Client::CreateProduct2(void) noexcept
+{
+	return this->current_factory->CreateProduct2();
+}
+#pragma endregion
+
+#pragma region TEST
+void TEST::DO_TEST(void)
+{
+	ConcreteFactory1 cf1; ConcreteFactory2 cf2;
+	Client client(cf1);
+
+	client.CreateProduct1()->SomeOperation();
+	client.CreateProduct2()->SomeOperation();
+
+	client.ChangeFactory(cf2);
+
+	client.CreateProduct1()->SomeOperation();
+	client.CreateProduct2()->SomeOperation();
+};
+#pragma endregion
---
+++ Algorithms/patterns/AbstractFactory.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#ifndef __ABSTRACT_FACTORY_HPP__
+#define __ABSTRACT_FACTORY_HPP__
+
+// for work demonstration
+#include <iostream>
+
+#pragma region Products
+class IProduct
+{
+public:
+	virtual void SomeOperation(void) = 0;
+};
+class ConcreteProductA1 : public IProduct
+{
+public:
+	virtual void SomeOperation(void) override final;
+};
+class ConcreteProductA2 : public IProduct
+{
+public:
+	virtual void SomeOperation(void) override final;
+};
+class ConcreteProductB1 : public IProduct
+{
+public:
+	virtual void SomeOperation(void) override final;
+};
+class ConcreteProductB2 : public IProduct
+{
+public:
+	virtual void SomeOperation(void) override final;
+};
+#pragma endregion
+
+#pragma region Factories
+class IAbstractFactory
+{
+public:
+	virtual IProduct* CreateProduct1(void) noexcept = 0;
+	virtual IProduct* CreateProduct2(void) noexcept = 0;
+};
+class ConcreteFactory1 : public IAbstractFactory
+{
+public:
+	virtual IProduct* CreateProduct1(void) noexcept override final;
+	virtual IProduct* CreateProduct2(void) noexcept override final;
+};
+class ConcreteFactory2 : public IAbstractFactory
+{
+public:
+	virtual IProduct* CreateProduct1(void) noexcept override final;
+	virtual IProduct* CreateProduct2(void) noexcept override final;
+};
+#pragma endregion
+
+#pragma region Client
+class Client
+{
+public:
+	Client(void) = default;
+	Client(IAbstractFactory& factory);
+	~Client(void) = default;
+
+	void ChangeFactory(IAbstractFactory& factory);
+
+	IProduct* CreateProduct1(void) noexcept;
+	IProduct* CreateProduct2(void) noexcept;
+private:
+	IAbstractFactory* current_factory;
+};
+#pragma endregion
+
+#pragma region TEST
+class TEST
+{
+public:
+	// use TEST::DO_TEST() for test
+	static void DO_TEST(void);
+};
+#pragma endregion
+
+#endif // !__ABSTRACT_FACTORY_HPP__
