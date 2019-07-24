9394137537f308fd667dea12aae5e7e5382ceed0
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-24 16:51:02 +0300

    Added Proxy implementation

---
+++ Algorithms/patterns/structural/Proxy.cpp
@@ -0,0 +1,46 @@
+#include "Proxy.hpp"
+
+void Proxy::Service::SomeOperation(void)
+{
+	std::cout << "\nService, doing some operation...";
+}
+
+Proxy::Proxy::Proxy(void)
+{
+	this->service = new Service();
+	this->is_alloc = true;
+}
+Proxy::Proxy::Proxy(Service* service)
+{
+	this->service = service;
+	this->is_alloc = false;
+}
+Proxy::Proxy::~Proxy(void)
+{
+	if (this->is_alloc)
+		delete this->service;
+}
+
+void Proxy::Proxy::SomeOperation(void)
+{
+	this->SomeAnotherOperation();
+	this->service->SomeOperation();
+}
+void Proxy::Proxy::SomeAnotherOperation(void)
+{
+	std::cout << "\nDoing some another operation...";
+}
+
+#pragma region TEST
+void Proxy::TEST::DO_TEST(void)
+{
+	IServiceInterface* service;
+	
+	service = new Service();
+	service->SomeOperation();
+
+	delete service;
+	service = new Proxy();
+	service->SomeOperation();
+};
+#pragma endregion
---
+++ Algorithms/patterns/structural/Proxy.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef __PROXY_HPP__
+#define __PROXY_HPP__
+
+// for work demonstration
+#include <iostream>
+
+namespace Proxy
+{
+	class IServiceInterface
+	{
+	public:
+		virtual void SomeOperation(void) = 0;
+	};
+	
+	class Service : public IServiceInterface
+	{
+	public:
+		virtual void SomeOperation(void) override final;
+	};
+
+	class Proxy : public IServiceInterface
+	{
+	public:
+		Proxy(void);
+		Proxy(Service* service);
+		~Proxy(void);
+
+		virtual void SomeOperation(void) override final;
+		void SomeAnotherOperation(void);
+	private:
+		Service* service; bool is_alloc;
+	};
+
+	class TEST
+	{
+	public:
+		// use TEST::DO_TEST() for test
+		static void DO_TEST(void);
+	};
+};
+
+#endif // !__PROXY_HPP__
