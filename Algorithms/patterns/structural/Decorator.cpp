02e7b5e53ba456a15aae30a3b2a46295c997520e
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-24 17:30:44 +0300

    Added Decorator implementation

---
+++ Algorithms/patterns/structural/Decorator.cpp
@@ -0,0 +1,59 @@
+#include "Decorator.hpp"
+
+#pragma region Components
+void Decorator::ConcreteComponent::Execute(void)
+{
+	std::cout << "\nConcrete component 1: executing...";
+}
+#pragma endregion
+
+#pragma region Decorators
+Decorator::BaseDecorator::BaseDecorator(IComponent* wrappee)
+{
+	this->wrappee = wrappee;
+}
+void Decorator::BaseDecorator::Execute(void)
+{
+	this->wrappee->Execute();
+}
+
+Decorator::ConcreteDecorator1::ConcreteDecorator1(IComponent* wrappee) : BaseDecorator(wrappee) {}
+void Decorator::ConcreteDecorator1::Execute(void)
+{
+	__super::Execute();
+	this->DoExtraThing();
+}
+void Decorator::ConcreteDecorator1::DoExtraThing(void)
+{
+	std::cout << "\nConcrete decorator 1: doing extra thing...";
+}
+
+Decorator::ConcreteDecorator2::ConcreteDecorator2(IComponent* wrappee) : BaseDecorator(wrappee) {}
+void Decorator::ConcreteDecorator2::Execute(void)
+{
+	__super::Execute();
+	this->DoExtraThing();
+}
+void Decorator::ConcreteDecorator2::DoExtraThing(void)
+{
+	std::cout << "\nConcrete decorator 2: doing extra thing...";
+}
+#pragma endregion
+
+#pragma region TEST
+void Decorator::TEST::DO_TEST(void)
+{
+	IComponent* cc = new ConcreteComponent;
+	cc->Execute();
+
+	IComponent* decorated = new ConcreteDecorator1(cc);
+	decorated->Execute();
+
+	delete decorated;
+	decorated = new ConcreteDecorator2(cc);
+	decorated->Execute();
+
+	delete decorated;
+	delete cc;
+};
+#pragma endregion
---
+++ Algorithms/patterns/structural/Decorator.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#ifndef __DECORATOR_HPP__
+#define __DECORATOR_HPP__
+
+// for work demonstration
+#include <iostream>
+
+namespace Decorator
+{
+#pragma region Components
+	class IComponent
+	{
+	public:
+		virtual void Execute(void) = 0;
+	};
+
+	class ConcreteComponent : public IComponent
+	{
+	public:
+		virtual void Execute(void) override final;
+	};
+#pragma endregion
+
+#pragma region Decorators
+	class BaseDecorator : public IComponent
+	{
+	public:
+		BaseDecorator(void) = delete;
+		BaseDecorator(IComponent* wrappee);
+		~BaseDecorator(void) = default;
+
+		virtual void Execute(void) override;
+	private:
+		IComponent* wrappee;
+	};
+
+	class ConcreteDecorator1 : public BaseDecorator
+	{
+	public:
+		ConcreteDecorator1(IComponent* wrappee);
+
+		virtual void Execute(void) override final;
+		void DoExtraThing(void);
+	};
+
+	class ConcreteDecorator2 : public BaseDecorator
+	{
+	public:
+		ConcreteDecorator2(IComponent* wrappee);
+
+		virtual void Execute(void) override final;
+		void DoExtraThing(void);
+	};
+#pragma endregion
+
+	class TEST
+	{
+	public:
+		// use TEST::DO_TEST() for test
+		static void DO_TEST(void);
+	};
+};
+
+#endif // !__DECORATOR_HPP__
