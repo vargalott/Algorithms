3c57ba86ed16049dcb8beff61cadf6f448453adb
vargalott <156085832+vargalott@users.noreply.github.com>
2019-07-27 16:17:30 +0300

    Added file for testing some C++2a features

---
+++ Algorithms/garbage/Feature20.cpp
@@ -0,0 +1,29 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+/*
+*	$ vcvars64.bat where using cl.exe
+*	**********************************************************************
+*	** Visual Studio 2019 Developer Command Prompt v16.2.0
+*	** Copyright (c) 2019 Microsoft Corporation
+*	**********************************************************************
+*	[vcvarsall.bat] Environment initialized for: 'x64'
+*
+*	Compiled with #see more precisely for each item#
+*
+*	Copyright(c) 2019 Mykola Symon. All rights reserved.
+*
+*	Assemble of some C++20 featuers
+*/
+
+/*
+*	P0614R1 http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0614r1.html 
+*	Test done
+*	clang -std=c++2a filename
+*/
+
+/*
+*	P0482R6 http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p0482r6.html
+*	Test done
+*	g++ -std=c++2a filename
+*/
+
